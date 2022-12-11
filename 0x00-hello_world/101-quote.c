da3ec7ab35ccefd12011740a52f4d2d4e42b917b
steven192 <stevenduodu192@gmail.com>
2022-12-11 14:18:17 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main -prints exactly and that piece of art is useful
  *
- * Return: Always 1 (Success)
+ * Return: Always 1
  */
 
 int main(void)
 
 {
-	write(1, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
