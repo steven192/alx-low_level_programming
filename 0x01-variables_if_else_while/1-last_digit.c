757260f0b51ce71095b358d9ba245c15bb080b09
steven192 <stevenduodu192@gmail.com>
2022-12-12 04:19:11 -0800

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 /**
- * main - main block
- * Description: function to generate a random number
- * Return: 0
+ * main - main  function to generate a random number
+ *
+ * Return: Always (0) (Success)
+ *
  */
 int main(void)
 {
