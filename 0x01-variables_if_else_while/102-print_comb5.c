3cc8cf1672edee93dcb223cb136fb4e2dc90e347
steven192 <stevenduodu192@gmail.com>
2022-12-12 13:05:46 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,43 +8,31 @@
  */
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
+	int c;
+	int f;
 
-	int c2;
-	int f_d2;
-	int l_d2;
-
-	while (c2 <= 98)
-
+	for (c = 0; c < 100; c++)
 	{
-		f_d2 = (c2 / 10 + '0');
-		l_d2 = (c2 % 10 + '0');
-		c2  = 0;
-		while (c <= 98)
+		for (f = 0; f < 100; f++)
 		{
-			f_d = (c / 10 + '0');
-			l_d = (c % 10 + '0');
 
-			if (c < c2)
+			if (c < f)
 			{
-				putchar(f_d);
-				putchar(l_d);
+				putchar(c);
+				putchar(c);
 				putchar (' ');
-				putchar(f_d2);
-				putchar(l_d2);
-
-				if (c != 98)
+				putchar(f);
+				putchar(f);
+				if (c != 98 || f != 99)
 				{
-					putchar(';');
+					putchar(',');
 					putchar(' ');
 				}
 			}
-			c2++;
 		}
-		c++;
 	}
+
+
 	putchar('\n');
 	return (0);
 }
