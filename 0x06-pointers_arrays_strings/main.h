ee08ba8725c764815cd805eafc08fbc2bea9979a
steven192 <stevenduodu192@gmail.com>
2022-12-22 09:06:15 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define FILE_MAIN
 
 int _putchar(char c);
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -16,4 +17,3 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 #endif
-
