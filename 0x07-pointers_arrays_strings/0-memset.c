53c6201935bed7fef6d3e3bf50968ccfee428595
steven192 <stevenduodu192@gmail.com>
2023-01-04 07:33:37 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,20 @@
 #include "main.h"
+
 /**
- * _memset -fils memeory with a constant byte
- * @s:source string
- * @b:the constant byte for filling
- * @n:length of buffer
- * Return:new string
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
+	unsigned int i;
 
-	unsigned int i = 0;
-
-	for (i = 0; i < n ; i++)
-{
-	s(i) = b;
-}
-return (s);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
