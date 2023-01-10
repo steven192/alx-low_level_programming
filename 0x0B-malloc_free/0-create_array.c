41d79b8bceae00fd5227adc75a76d7eb5e5747dd
steven192 <stevenduodu192@gmail.com>
2023-01-10 15:09:40 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,3 +16,25 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
+
+	/*Define values with malloc*/
+	buffer = (char *) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		position = 0;
+		while (position < size) /*While for array*/
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+
+		return (buffer);
+	}
+
+}
