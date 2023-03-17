a50d8cc2cc556a4d75c091102783be008ccc6359
steven192 <stevenduodu313@gmail.com>
2023-03-16 22:21:49 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 
+
 /**
  * print_dlistint - prints all the elements of a dlistint_t list.
  * @h: pointer to head of the list
@@ -10,7 +11,6 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t size;
 
 	size = 0;
-
 	if (h == NULL)
 	return (size);
 
