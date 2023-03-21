e19012dcfcfbda0a9f35b25c793bf60d80078e4c
steven192 <stevenduodu313@gmail.com>
2023-03-20 18:02:16 -0700

    task 0

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isupper - check the code for Holberton School students.
+ * @c: this is the check
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
+}
